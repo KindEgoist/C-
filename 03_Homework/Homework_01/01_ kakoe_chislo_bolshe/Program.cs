dce822753090d0d888ed2d8825b0791ecb8d1ce3
KindEgoist <djondevil@gmail.com>
2022-08-20 18:45:40 +0700

    Home 01

---
+++ 03_Homework/Homework_01/01_ kakoe_chislo_bolshe/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+Console.Clear();
+Console.SetCursorPosition(40, 1);
+Console.WriteLine("==Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее==");
+Console.Write("Введите число X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > y)
+{
+    Console.Write($"Число X:{x} больше числа Y:{y} ");
+}
+else
+{
+    Console.Write($"Число Y:{y} больше числа X:{x} ");
+}
---
+++ 03_Homework/Homework_01/04_chetnoe_N/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.Clear();
+Console.SetCursorPosition(40, 1);
+Console.WriteLine("==Программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.==");
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+while(current <= N)
+{   
+    if (current %2 == 0)
+    {
+                Console.Write(current + " ");
+    }
+    current ++;
+}
+
+
