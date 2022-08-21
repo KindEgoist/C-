46881e7abc41ba7ecc1bf9b7e219772551bd80ef
KindEgoist <djondevil@gmail.com>
2022-08-21 10:57:21 +0700

    Homework 01 up

--- 03_Homework/Homework_01/04_chetnoe_N/Program.cs
+++ 03_Homework/Homework_01/04_chetnoe_N/Program.cs
@@ -5,7 +5,7 @@
 
 Console.Clear();
 Console.SetCursorPosition(40, 1);
-Console.WriteLine("==Программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.==");
+Console.WriteLine("==Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.==");
 Console.Write("Введите число N: ");
 int N = Convert.ToInt32(Console.ReadLine());
 int current = 1;
