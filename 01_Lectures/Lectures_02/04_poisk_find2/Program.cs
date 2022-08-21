253e1d904041b78286ed6de403fa63c7af497296
KindEgoist <djondevil@gmail.com>
2022-08-21 10:56:01 +0700

    Lectures 02

---
+++ 01_Lectures/Lectures_02/04_poisk_find2/Program.cs
@@ -0,0 +1,28 @@
+﻿//метод заполнения массива
+
+void FillArray(int[] collection) //void не возвращает данные массива
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10]; //определение саммива из 10 элементов
+
+FillArray(array); //заполнение массива
+PrintArray(array); //распечатка массива
