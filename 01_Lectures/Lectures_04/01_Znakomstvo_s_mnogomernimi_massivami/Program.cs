d4385d95154e2b87bbf3e453e7ac7d92b878730f
KindEgoist <djondevil@gmail.com>
2022-09-04 20:31:27 +0700

    Лекция №4

---
+++ 01_Lectures/Lectures_04/01_Znakomstvo_s_mnogomernimi_massivami/Program.cs
@@ -0,0 +1,53 @@
+﻿
+/* string[,] table = new string[2, 5];//иницилизация
+table [1,2] = "слово";//обращение к нужному элементу
+
+for(int i = 0; i <2; i++)
+{
+    for(int j = 0; j < 5; j++)
+    {
+        Console.Write($"-{table[i, j]}-");
+    }
+Console.WriteLine();
+}*/
+
+/*
+int[,] matrix = new int[3,4];
+
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+Console.WriteLine();
+} */
+
+void Print(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+        Console.Write($"{matrix[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void Fill(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+int[,] matrix = new int[3,4];
+Print(matrix);
+Fill(matrix);
+Console.WriteLine();
+Print(matrix);
