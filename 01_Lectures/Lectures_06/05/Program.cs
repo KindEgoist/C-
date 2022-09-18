db7f966b490d4731576cbf0ae0f12cd02f167365
KindEgoist <djondevil@gmail.com>
2022-09-18 19:19:43 +0700

    Лекция №6

---
+++ 01_Lectures/Lectures_06/05/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Перебор слов
+В некотором машинном алфавите имеются четыре буквы "а", "и", "с" и "в". Покажите все слова, состоящие из Т букв, которые можно построить из букв этого алфавита. */
+
+int n = 1;
+void FindWorlds(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word) }"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWorlds(alphabet, word, length +1);
+    }
+}
+
+FindWorlds("аисв", new char[2]);
---
+++ 01_Lectures/Lectures_06/06/Program.cs
@@ -0,0 +1,35 @@
+﻿// как посмотреть содержимое папки?
+/*
+//содержимое папки
+string path = "D:/!Musik";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}*/
+//под каталоги
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo [] catalogs = catalog.GetDirectories();
+    for(int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+
+
+    FileInfo[] files = catalog.GetFiles();
+
+    for ( int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+
+string path = @"D:/!Musik";
+CatalogInfo(path);
